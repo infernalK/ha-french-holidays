a99f08a16659543f89783173a21ae94165e6df9e
infernalK <59821935+infernalK@users.noreply.github.com>
2026-04-21 17:05:52 +0200

    add vacances_fr_today

--- custom_components/french_holiday/binary_sensor.py
+++ custom_components/french_holiday/binary_sensor.py
@@ -44,7 +44,14 @@ async def async_setup_entry(
                     key="vacances_fr_tomorrow",
                     name=f"{FRIENDLY_PREFIX} demain ?",
                 ),
-            )
+            ),
+            VacancesFrTodayBinarySensor(
+                coordinator=entry.runtime_data.coordinator,
+                entity_description=BinarySensorEntityDescription(
+                    key="vacances_fr_today",
+                    name=f"{FRIENDLY_PREFIX} aujourd'hui ?",
+                ),
+            ),
         ]
     )
 
@@ -92,6 +99,57 @@ class VacancesFrTomorrowBinarySensor(VacancesFrEntity, BinarySensorEntity):
             if event_start <= tomorrow < event_end:
                 self._attr_is_on = True
                 self._attr_extra_state_attributes = get_period_extra_attributes(tomorrow_event)
+            else:
+                self._attr_is_on = False
+        else:
+            self._attr_is_on = False
+
+        self.schedule_update_ha_state()
+
+
+class VacancesFrTodayBinarySensor(VacancesFrEntity, BinarySensorEntity):
+    """vacances_fr today binary_sensor class."""
+
+    unsubscribe: Callable[[], None] | None = None
+
+    def __init__(
+        self,
+        coordinator: VacancesFrDataUpdateCoordinator,
+        entity_description: BinarySensorEntityDescription,
+    ) -> None:
+        """Initialize the binary_sensor class."""
+        super().__init__(coordinator)
+        self.entity_id = f"{Platform.BINARY_SENSOR}.{DOMAIN}_today_{
+            slugify(self.coordinator.config_entry.data['zone'])
+        }"
+        self.entity_description = entity_description
+
+    async def async_added_to_hass(self) -> None:
+        """Subscribe to event each day at 00:00 to update value."""
+        await super().async_added_to_hass()
+        self.unsubscribe = async_track_time_change(
+            self.hass, lambda _: self._handle_coordinator_update(), 0, 0, 0
+        )
+
+    async def async_will_remove_from_hass(self) -> None:
+        """Unsubscribe from the events."""
+        if self.unsubscribe:
+            self.unsubscribe()
+        await super().async_will_remove_from_hass()
+
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        """Update the entity."""
+        today = dt.now().date()
+        today_event = self.coordinator.get_date_event(today)
+        
+        if today_event is not None:
+            event_start = today_event.start
+            event_end = today_event.end 
+
+            if event_start <= today <= event_end:
+                self._attr_is_on = True
+                self._attr_extra_state_attributes = get_period_extra_attributes(today_event)
             else:
                 self._attr_is_on = False
         else:
